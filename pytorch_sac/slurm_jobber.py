f07345aa048b948cb0f4bc1506c9f2cf1ceed512
Anthony Liang <aliangdw@umich.edu>
2022-09-21 00:10:38 -0700

    some changes, running SAC on single tasks, keep hand init state the same

--- pytorch_sac/slurm_jobber.py
+++ pytorch_sac/slurm_jobber.py
@@ -7,16 +7,19 @@ import glob
 from sklearn.model_selection import ParameterGrid
 from metaworld.envs import ALL_V2_ENVIRONMENTS_GOAL_OBSERVABLE
 
+
+PROGRAMS_PER_JOB = 8
+
 # header for slurm, need to edit this
 HEADER = """#!/bin/bash 
 #SBATCH --job-name=train_dt_adapters
 #SBATCH --partition=debug
 #SBATCH --nodes=1
-#SBATCH --time=5:00:00
-#SBATCH --cpus-per-task=5
-#SBATCH --gpus-per-node=1080:1
-#SBATCH --nodelist=ink-ron
-#SBATCH --output=/home/anthony/dt_adapters/slurm_output/%j.out
+#SBATCH --time=12:00:00
+#SBATCH --cpus-per-task=10
+#SBATCH --gpus-per-node=2080:1
+#SBATCH --nodelist=ink-ellie
+#SBATCH --output=/home/anthony/dt_adapters/pytorch_sac/slurm_output/%j.out
 
 HOME=/home/anthony
 
@@ -33,37 +36,46 @@ export TOKENIZERS_PARALLELISM=false
 os.makedirs("slurm_files", exist_ok=True)
 
 # create param grid
-envs = list(ALL_V2_ENVIRONMENTS_GOAL_OBSERVABLE.keys())[:3]
-print(envs)
-experiments = [f"mw-dc-{env}" for env in envs]
+envs = list(ALL_V2_ENVIRONMENTS_GOAL_OBSERVABLE.keys())
 
 # param_grid = {"env": envs, "experiment": experiments, "log_to_wandb": ["true"]}
 # configs = list(ParameterGrid(param_grid))
 configs = []
 
-for (env, experiment) in zip(envs, experiments):
-    if os.path.exists(f"outputs/{experiment}"):
-        print(f"skipping {experiment} because output file already exists")
-    configs.append({"env": env, "experiment": experiment, "log_to_wandb": "true"})
+skip = []
+for env in envs:
+    # if os.path.exists(f"outputs/{experiment}"):
+    #     print(f"skipping {experiment} because output file already exists")
+    #     skip.append(env)
+    configs.append({"env": env, "log_to_wandb": "true"})
 
 # create individual slurm files
-for cfg in configs:
-    # print(cfg)
-    slurm_cmd = HEADER + "CUDA_VISIBLE_DEVICES=0 DISPLAY=:0 python3 train.py "
+# chunk configs
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i : i + n]
 
-    key = ""
-    for k, v in cfg.items():
-        slurm_cmd += f"{k}={v} "
-        key += f"_{k}_{v}"
 
+chunks = list(chunks(configs, PROGRAMS_PER_JOB))
+
+for i, chunk in enumerate(chunks):
+    slurm_cmd = HEADER
+    for j, cfg in enumerate(chunk):
+        slurm_cmd += "CUDA_VISIBLE_DEVICES=0 DISPLAY=:0 python3 train.py "
+        key = ""
+        for k, v in cfg.items():
+            slurm_cmd += f"{k}={v} "
+            key += f"_{k}_{v}"
+
+        if j != len(chunk) - 1:
+            slurm_cmd += "&\n"
+
+    slurm_cmd = slurm_cmd[:-1]  # remove last space
     # print(slurm_cmd)
-    with open(os.path.join("slurm_files", f"run_train{key}.slrm"), "w") as f:
+    file = os.path.join("slurm_files", f"run_train_{i}.slrm")
+    with open(file, "w") as f:
         f.write(slurm_cmd)
 
-# iterate through and run the files
-files = glob.glob("slurm_files/*")
-print(f"running {len(files)} slurm jobs")
-
-for file in files:
-    print(f"running: sbatch {file}")
-    os.system(f"sbatch {file}")
+    # print(f"running: sbatch {file}")
+    # os.system(f"sbatch {file}")
